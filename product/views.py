a5525ade95813d10c1d81772148132b0a5291cf0
anketmanoj <anket.m@live.com>
2022-02-26 17:57:36 +0400

    serializers for django done

--- product/views.py
+++ product/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .serializers import ProductSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Product
 
 # Create your views here.
+
+
+class LatestProductsList(APIView):
+    def get(self, request):
+        products = Product.objects.all()[:10]
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
